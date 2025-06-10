77832403628d4404237957f6c102901c12556b27
Carlos Huguez <carlos.huguez@sonorasoft.com>
2025-06-10 14:51:21 -0700

    popToTop

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { globalStyles } from '../../theme';
+import { Button } from '../../components';
+import { type  NavigationProp, StackActions, useNavigation } from '@react-navigation/native';
+import { type  RootStackParam } from '../../routes';
 
 const SettingsScreen = () => {
+   
+   const navigation = useNavigation<NavigationProp<RootStackParam>>()
+
    return (
-      <View>
-         <Text> SettingsScreen </Text>
+      <View style={ globalStyles.container }>
+         <Text > Settings Screen </Text>
+         <Button 
+            type='primary'
+            onClick={ () => navigation.canGoBack() && navigation.goBack() }
+            label={ "go Back" }
+         />
+
+         <Button 
+            type='primary'
+            onClick={ () => navigation.dispatch( StackActions.popToTop() )}
+            label={ "go Home" }
+         />
+
       </View>
    )
 }
